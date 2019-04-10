dd5cc7b6d7270cafb5111bdc849104fe0e423eda
radhakrishna <radha784578@gmail.com>
2019-04-10 11:22:15 +0530

    given  login validations

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import {apiService} from '../apiService';
+import { apiService } from '../apiService';
 import { Router } from '@angular/router';
-import { HttpClientModule,HttpClient } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -14,88 +14,111 @@ export class HomePage {
   password: string;
   message: string;
   // register Data .. password is there  duplicates are error 
-  name:string;
-  email:string;
-  phone:number;
+  name: string;
+  email: string;
+  phone: number;
 
   public loginButton: boolean = true;
   public signUpButton: boolean = false;
-   // error messages 
-   public userEmailMessage: string;
-   private loginData:any;
-  
+  // error messages 
+  public userEmailMessage: string;
+  private loginData: any;
 
-  constructor(private router: Router,private http:HttpClient) { }
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   submit() {
-     // for login 
-      // get the data from db  with validation 
-      console.log(apiService.login + '"'+this.username+'"');
-      this.http.get(apiService.login + '"'+this.username+'"').subscribe(data=>{
-                 this.loginData = data;
-       // console.log(this.loginData.message.rows[0].value);
-       if (this.loginData.message.rows[0].value._id == this.username && this.loginData.message.rows[0].value.password == this.password) {
-        this.message = " Login success!!!!!";
-        //this.router.navigateByUrl('/location');
-        //this.router.navigateByUrl('/simData');
+    this.message = " Fetching Login Details ..........";
+    // for login 
+    // get the data from db  with validation 
+    //console.log(apiService.login + this.username);
+    this.http.get(apiService.login + this.username).subscribe(data => {
+      this.loginData = data;
+     // console.log(data);
+
+      if (this.loginData == null) {
+        this.message = " Your not a Existing Usere.. ";
+      } else {
+
+      
+          // console.log(this.loginData.message.rows[0].value);
+          if (this.loginData._id == this.username && this.loginData.password == this.password) {
+          
+            if (this.loginData.state == "active") {
+             
+                  if(this.loginData._id =="admin"){
+                    this.message = "Admin Login success!!!!!";
+                  }else{
+                    this.message = this.loginData._id + "Login success!!!!!";
+                  }
+               //this.router.navigateByUrl('/location');
+               //this.router.navigateByUrl('/simData');
+            }else{
+              this.message = "Your Account is Inactive.Kindly Contact Admin.";
+            }
+           
+          }
+          else {
+            this.message = "Oops!! Username or password is Wrong";
+          }
       }
-      else {
-        this.message = "Oops!! Username or password is Wrong";
-      }
-});
-    
+    });
+
   }
   login() {
     // only  for sisplay the login feilds 
     this.loginButton = true;
     this.signUpButton = false;
-        
+
   }
   signUp() {
     this.loginButton = false;
     this.signUpButton = true;
   }
-  RegisterSubmit(){
-   
-           if(this.name == undefined || this.name == "" || this.name == null){
-            this.userEmailMessage = 'Please Enter Name';
-           }
-            else if(this.email == undefined || this.email == "" || this.email == null){
-            this.userEmailMessage = 'Please Enter email';
-           }
-            else if(this.phone == undefined || this.phone == 0 || this.phone == null){
-            this.userEmailMessage = 'Please Enter phone';
-           }
-            else if(this.password == undefined || this.password == "" || this.password == null){
-            this.userEmailMessage = 'Please Enter password';
-           }else{
-            this.userEmailMessage = 'Storing Data....';
-           this.http.post(apiService.signUp,
-                    { _id:this.email,
-                      name:this.name,
-                      email:this.email,
-                      phone:this.phone,
-                      password:this.password}).subscribe(
-                                      data => {
-                                    console.log("POST Request is successful ", data);
-                                    this.userEmailMessage = 'successfully Storing Data ..';
-                                    this.reset();
-                                    },
-                                    error  => {
+  RegisterSubmit() {
 
-                                    console.log("Error", error);
+    if (this.name == undefined || this.name == "" || this.name == null) {
+      this.userEmailMessage = 'Please Enter Name';
+    }
+    else if (this.email == undefined || this.email == "" || this.email == null) {
+      this.userEmailMessage = 'Please Enter email';
+    }
+    else if (this.phone == undefined || this.phone == 0 || this.phone == null) {
+      this.userEmailMessage = 'Please Enter phone';
+    }
+    else if (this.password == undefined || this.password == "" || this.password == null) {
+      this.userEmailMessage = 'Please Enter password';
+    } else {
+      this.userEmailMessage = 'Storing Data....';
+      this.http.post(apiService.signUp,
+        {
+          _id: this.email,
+          name: this.name,
+          email: this.email,
+          phone: this.phone,
+          password: this.password,
+          state: "inActive"
+        }).subscribe(
+          data => {
+            console.log("POST Request is successful ", data);
+            this.userEmailMessage = 'successfully Storing Data ..';
+            this.reset();
+          },
+          error => {
 
-                                     });
-                                    }
-    
+            console.log("Error", error);
+
+          });
+    }
+
   }
-   reset(){
-    this.name ='',
-    this.email='';
-    this.phone=0;
-    this.password ='';
+  reset() {
+    this.name = '',
+      this.email = '';
+    this.phone = 0;
+    this.password = '';
 
-   }
+  }
   onChange() {
     this.message = "";
     this.userEmailMessage = "";
