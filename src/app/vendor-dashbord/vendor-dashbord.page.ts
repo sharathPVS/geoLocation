227a16ce5b9bdbf17b086713af66571ec26fb4cf
radhakrishna <radhakrishna@whiznext.com>
2019-04-29 10:39:25 +0530

    update call

--- src/app/vendor-dashbord/vendor-dashbord.page.ts
+++ src/app/vendor-dashbord/vendor-dashbord.page.ts
@@ -27,6 +27,7 @@ export class vendorDashboardPage implements OnInit {
   public adminData: any;
   public addEmployee: boolean = false;
   public getEmployeeList: boolean = false;
+  public UpdateDataFeilds:boolean = false;
 
   public name: string;
   public Phone: number;
@@ -37,17 +38,21 @@ export class vendorDashboardPage implements OnInit {
   public cellId: string;
   private LoginCollection: string;
   public vendorName: string;
+  public simNumber:number;
 
+  // update button value 
+   public updateButtonValue:string = "Update";
 
   ngOnInit() {
     this.userMessage = '';
-    this.vendorName = loginUserData.getLoginUserData().company
+    this.vendorName =  (loginUserData.getLoginUserData() == undefined) ? '' : loginUserData.getLoginUserData().company
   }
 
   createEmployee() {
     this.addEmployee = true;
     this.getEmployeeList = false;
     this.userMessage = '';
+    this.UpdateDataFeilds = false;
 
   };
   getAllEmployees() {
@@ -59,6 +64,7 @@ export class vendorDashboardPage implements OnInit {
     this.getEmployeeList = true;
     this.addEmployee = false;
     this.userMessage = '';
+    this.UpdateDataFeilds = false;
 
   }
 
@@ -75,12 +81,15 @@ export class vendorDashboardPage implements OnInit {
       this.userMessage  = "Required AAdhar Number";
 
      }else{
+       let url = apiService.createUserColletionDB + '?collection=' + this.LoginCollection;
 
-    this.http.post(apiService.createUserColletionDB + '?collection=' + this.LoginCollection, {
+       //console.log('store',url)
+    this.http.post(url,{
       _id: this.Phone,
       name: this.name,
       Phone: this.Phone,
       aadharCardNumber: this.aadharCardNumber,
+      simNumber:this.simNumber,
       mcc: this.mcc,
       mnc: this.mnc,
       lac: this.lac,
@@ -104,6 +113,7 @@ export class vendorDashboardPage implements OnInit {
     this.mnc
     this.cellId = "";
     this.lac = '';
+    this.simNumber  = 0 ;
   }
   onChange() {
     this.userMessage = '';
@@ -135,23 +145,40 @@ export class vendorDashboardPage implements OnInit {
   }
 
   // update  employee data 
-  update(updateData){
 
-    console.log("updated data" ,updateData);
+  edit(val){
+    console.log(val);
+
+    this.getEmployeeList = false;
+    this.UpdateDataFeilds = true;
+
+    this.name = (val  == undefined || null || "") ? "" : val.name;
+    this.Phone = (val  == undefined || null || "" || 0) ? "" : val.Phone;
+    this.aadharCardNumber = (val  == undefined || null || "" || 0) ? "" : val.aadharCardNumber;
+    this.mcc = (val  == undefined || null || "" || 0) ? "" : val.mcc;
+    this.mnc = (val  == undefined || null || "" || 0) ? "" : val.mnc;
+    this.cellId = (val == undefined || null || "" || 0) ? "" : this.handleData.message.cellId;
+    this.lac = (this.handleData.message  == undefined || null || "" ) ? "" : val.lac;
+    this.simNumber  = (val  == undefined || null || "" || 0) ? "" : val.simNumber;
+screenLeft
+  }
+  update() {
     let url = apiService.updateOrgData + '?collection=' + this.LoginCollection;
     //console.log(url);
-    this.http.put(url,updateData).subscribe(data => {
-      this.handleData = data;
-     // console.log(this.handleData.message);
-      if (this.handleData.message.length == 0) {
-        this.userMessage = " oops! no records are found.";
-        this.getEmployeeList = false;
-      } else {
-        this.userMessage = '';
-        this.adminData = this.handleData.message;
-
-        // console.log('final Data', this.adminData[0].name);
-      }
+    this.http.put(url,{_id: this.Phone,
+      name: this.name,
+      Phone: this.Phone,
+      aadharCardNumber: this.aadharCardNumber,
+      simNumber:this.simNumber,
+      mcc: this.mcc,
+      mnc: this.mnc,
+      lac: this.lac,
+      cellId: this.cellId
+    }).subscribe(data => {
+      this.handleData = data
+      this.userMessage =  this.handleData.message ;
+  
+    
     })
   }
 
