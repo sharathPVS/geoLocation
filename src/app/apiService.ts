6e4c6d07534757693dfbe1dcc583e34ce4e11d99
radhakrishna <radhakrishna@whiznext.com>
2019-05-23 17:35:08 +0530

    given schedule timer

--- src/app/apiService.ts
+++ src/app/apiService.ts
@@ -1,6 +1,6 @@
 // this belongs to c DB
 //let url  = 'http://18.188.66.126:8888/';
-let url = "http://localhost:6003/";
+let url = "http://192.168.1.10:6003/";
 
 export let apiService = {
       login: url + 'login?id=',
@@ -18,7 +18,7 @@ export let apiService = {
       fetchSimData: url + 'fetchSimData?simNumber=',
       updateSimData: url + 'updateSimData',
       createNewSimdata: url + 'createNewSimdata',
-      storeGeoLocation:url + 'StoreGeoLocation'
+      storeGeoLocation: url + 'StoreGeoLocation'
 
 
 };
